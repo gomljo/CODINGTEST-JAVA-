dac367c8c9d33b17712571f41a666fbe6adcedbb
Jong Wook Lee <58705161+gomljo@users.noreply.github.com>
2023-12-15 21:55:26 +0900

    [level 2] Title: 모음 사전, Time: 9.49 ms, Memory: 69.6 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Solution {
@@ -24,20 +25,12 @@ public class Solution {
 
 
     public int solution(String word) {
-        int answer = 0;
-
         // 1개로 이루어졌을 때부터 5개로 이루어졌을 때까지 문자열을 만든 뒤에 인덱스로 접근
         List<String> current = new ArrayList<>();
         boolean[] visited = new boolean[MAX_DEPTH];
         makeWord(0, current, visited);
-        for (int i = 0; i < dictionary.size(); i++) {
-            if(word.equals(dictionary.get(i))){
-                answer = i+1;
-                break;
-            }
-        }
-        return answer;
+        dictionary.sort(Comparator.naturalOrder());
+        return dictionary.indexOf(word)+1;
     }
-
-
+    
 }
