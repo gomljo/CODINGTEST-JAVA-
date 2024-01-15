7f442da5c1d6048e4ac6a2e69cf61c444145f9a0
Jong Wook Lee <58705161+gomljo@users.noreply.github.com>
2024-01-15 14:55:59 +0900

    [Gold V] Title: 빗물, Time: 144 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -1,38 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int maxHeight = Integer.parseInt(st.nextToken());
+        int maxLength = Integer.parseInt(st.nextToken());
+        int[] contourLine = new int[maxLength];
 
-        String[] params = br.readLine().split(" ");
-        int H = Integer.parseInt(params[0]);
-        int W = Integer.parseInt(params[1]);
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < maxLength; i++) {
+            contourLine[i] = Integer.parseInt(st.nextToken());
+        }
         int answer = 0;
-        int[] blocks = Arrays.stream(br.readLine().split(" "))
-                .map(Integer::valueOf)
-                .mapToInt(block -> block)
-                .toArray();
-        for (int i = 1; i < W - 1; i++) {
+        for (int i = 1; i < maxLength; i++) {
             int left = 0;
             int right = 0;
 
-            for (int j = 0; j <= i; j++) {
-                left = Math.max(left, blocks[j]);
+            for (int j = 0; j < i; j++) {
+                left = Math.max(left, contourLine[j]);
             }
 
-            for (int j = i + 1; j < W; j++) {
-                right = Math.max(right, blocks[j]);
-            }
-            if(blocks[i] < left && blocks[i] < right){
-                answer += Math.min(left, right) - blocks[i];
+            for (int j = i + 1; j < maxLength; j++) {
+                right = Math.max(right, contourLine[j]);
             }
 
+            int height = Math.min(left, right);
+
+            if (contourLine[i] < height) {
+                answer += height - contourLine[i];
+            }
         }
-
         System.out.println(answer);
     }
 }
