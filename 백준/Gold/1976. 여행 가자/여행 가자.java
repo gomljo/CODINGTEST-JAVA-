a8f73969242c3fb3e02936aefe2d40aab80b5ac4
Jong Wook Lee <58705161+gomljo@users.noreply.github.com>
2024-02-05 15:52:22 +0900

    [Gold IV] Title: 여행 가자, Time: 208 ms, Memory: 18764 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -5,6 +5,16 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
+
+    // 문제에 주어진 예제를 확인해보면
+    // 주어진 여행 계획대로 움직이는 것이 가능한 경우도 존재하고 포함되는 경우도 존재한다.
+    // 즉, 주어진 여행 계획에 포함된 도시들끼리의 연결 여부를 확인해야 하는 문제이다.
+    // 도시들 간의 연결 여부는 프로그래밍 차원에서 노드의 연결 여부를 확인해야 한다.
+    // 그렇다면 연결 여부를 어떻게 알 수 있을까?
+    // 문제 유형에 나타나 있는 분리 집합(disjoint set)을 이용해야 한다.
+    // 분리 집합은 union-find 알고리즘으로 구할 수 있다.
+    // 
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -22,7 +32,7 @@ public class Main {
                 if (connectedOrNot == 1) {
                     union(i+1, j+1, parent);
                 }
-                
+
             }
         }
         List<Integer> plans = Arrays.stream(br.readLine().split(" "))
@@ -43,17 +53,18 @@ public class Main {
             System.out.println("NO");
         }
     }
-
+    // 연결된 노드들의 부모 노드를 합침
     public static void union(int a, int b, int[] parent) {
         int aParent = find(a, parent);
         int bParent = find(b, parent);
         if(aParent != bParent){
-            parent[bParent] = aParent;
+            parent[aParent] = bParent;
         }
     }
 
     public static int find(int x, int[] parent) {
         if (x != parent[x]) {
+            // 경로 압축
             parent[x] = find(parent[x], parent);
         }
         return parent[x];
