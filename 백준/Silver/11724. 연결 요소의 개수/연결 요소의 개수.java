083fbd8ba0b70cad43a0480ee346308206c7948a
Jong Wook Lee <58705161+gomljo@users.noreply.github.com>
2024-03-06 14:24:30 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 740 ms, Memory: 165208 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    private static boolean[] visited;
+    private static List<List<Integer>> graph;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int numberOfVertex = Integer.parseInt(st.nextToken());
+        int numberOfEdge = Integer.parseInt(st.nextToken());
+        graph = new ArrayList<>();
+
+        for (int i = 0; i <= numberOfVertex; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < numberOfEdge; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            graph.get(start).add(end);
+            graph.get(end).add(start);
+        }
+        visited = new boolean[numberOfVertex + 1];
+        int answer = 0;
+        for (int i = 1; i <= numberOfVertex; i++) {
+            if (!visited[i]) {
+                findConnectedComponent(0, i);
+                answer++;
+            }
+        }
+        System.out.println(answer);
+        br.close();
+    }
+
+    public static void findConnectedComponent(int numberOfElement, int startNode) {
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(startNode, 0));
+        while (!queue.isEmpty()) {
+            Node currentNode = queue.poll();
+            if (visited[currentNode.getEnd()]) {
+                continue;
+            }
+            visited[currentNode.getEnd()] = true;
+            List<Integer> adjacentNodeList = graph.get(currentNode.getEnd());
+            for (Integer adjacentNode : adjacentNodeList) {
+                if (!visited[adjacentNode]) {
+                    queue.add(new Node(adjacentNode, currentNode.getSize() + 1));
+                }
+            }
+        }
+    }
+}
+
+class Node {
+    private int end;
+    private int size;
+
+    public Node(int end, int size) {
+        this.end = end;
+        this.size = size;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
