71acb25409352a2b01a17cd56da89dceadcecf75
Jong Wook Lee <58705161+gomljo@users.noreply.github.com>
2024-03-29 23:12:27 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 616 ms, Memory: 148032 KB -BaekjoonHub

--- 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.java
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.java
@@ -9,14 +9,14 @@ public class Main {
         int n = scanner.nextInt();
         int k = n / 3;
         int coef = (int) (Math.log10(k) / Math.log10(2));
-        starMap = new char[n][n*2-1];
+        starMap = new char[n][n * 2 - 1];
         for (int i = 0; i < n; i++) {
             Arrays.fill(starMap[i], ' ');
         }
-        printStars(0, (n*2-1)/2, coef, -1, n);
+        printStars(0, (n * 2 - 1) / 2, coef, n);
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < 2*n-1; j++) {
+            for (int j = 0; j < 2 * n - 1; j++) {
                 sb.append(starMap[i][j]);
             }
             sb.append("\n");
@@ -25,14 +25,14 @@ public class Main {
 
     }
 
-    public static void printStars(int row, int col, int k, int flag, int height) {
+    public static void printStars(int row, int col, int k, int height) {
         if (k == 0) {
             print(row, col);
             return;
         }
-        printStars(row, col, k - 1, 0, height / 2);
-        printStars(row + height / 2, col - (height / 2), k - 1, 1, height / 2);
-        printStars(row + height / 2, col + (height / 2) , k - 1, 2, height / 2);
+        printStars(row, col, k - 1, height / 2);
+        printStars(row + height / 2, col - (height / 2), k - 1, height / 2);
+        printStars(row + height / 2, col + (height / 2), k - 1, height / 2);
     }
 
     public static void print(int row, int col) {
@@ -40,6 +40,7 @@ public class Main {
 
 
     }
+
     public static void eachPrint(int row, int col) {
 
         for (int i = 0; i < 3; i++) {
@@ -48,6 +49,6 @@ public class Main {
                 starMap[i + row][col + j] = '*';
             }
         }
-        starMap[row+1][col] = ' ';
+        starMap[row + 1][col] = ' ';
     }
 }
