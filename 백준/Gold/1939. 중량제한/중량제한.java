b932101517c67d07689ef7cfd5bd211e990837c6
Jong Wook Lee <58705161+gomljo@users.noreply.github.com>
2024-04-22 16:10:15 +0900

    [Gold III] Title: 중량제한, Time: 632 ms, Memory: 69248 KB -BaekjoonHub

--- 백준/Gold/1939. 중량제한/중량제한.java
+++ 백준/Gold/1939. 중량제한/중량제한.java
@@ -23,26 +23,25 @@ public class Main {
             graph.get(s).add(new Edge(e, w));
             graph.get(e).add(new Edge(s, w));
         }
+        MaximumWeightFinder finder = new MaximumWeightFinder(graph);
         st = new StringTokenizer(br.readLine());
         int factory = Integer.parseInt(st.nextToken());
         int otherFactory = Integer.parseInt(st.nextToken());
-        int start = 1;
-        int end = 1000000000;
-        while (start <= end) {
-            int mid = (start+end) / 2;
-
-            if(bfs(mid, factory, otherFactory)){
-                start = mid+1;
-            }
-            else {
-                end = mid-1;
-            }
-        }
-        System.out.println(end);
+        finder.calculate(factory, otherFactory);
+        finder.printMaxWeight();
         br.close();
     }
+}
+class MaximumWeightFinder {
+    private final List<List<Edge>> graph;
+    private int maxWeight;
 
-    public static boolean bfs(int weightLimit, int start, int end) {
+    public MaximumWeightFinder(List<List<Edge>> graph) {
+        this.graph = graph;
+        this.maxWeight = 0;
+    }
+
+    private boolean bfs(int weightLimit, int start, int end) {
         Queue<Edge> queue = new LinkedList<>();
         queue.add(new Edge(start, Integer.MIN_VALUE));
         boolean[] visited = new boolean[graph.size()+1];
@@ -61,8 +60,28 @@ public class Main {
         }
         return visited[end];
     }
-}
 
+    public void calculate(int factory, int otherFactory){
+        int minimumWeight = 1;
+        int maximumWeight = 1000000000;
+        while (minimumWeight <= maximumWeight) {
+            int mid = (minimumWeight+maximumWeight) / 2;
+
+            if(bfs(mid, factory, otherFactory)){
+                minimumWeight = mid+1;
+            }
+            else {
+                maximumWeight = mid-1;
+            }
+        }
+        this.maxWeight = maximumWeight;
+    }
+
+    public void printMaxWeight(){
+        System.out.println(this.maxWeight);
+    }
+
+}
 class Edge {
     private final int end;
     private final int weight;
